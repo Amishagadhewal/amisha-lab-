a271060bb97c3197dedf7ba9bdd83bdc196334f0
Python lab <84145797+amisha-lab@users.noreply.github.com>
2021-10-12 12:58:58 +0530

    Create Template.py

---
+++ Template.py
@@ -0,0 +1,8 @@
+#Write a program to fill in a letter template given below with name 
+and date:
+
+print('Enter name:')
+x = input()
+print('Enter date:')
+y = input()
+print('Dear ' + x + '\nYou have selected ' + y)
